becc8e6614393fb10e21c220855f396baea7410b
Guayaba <guayaba.rojo@gmail.com>
2024-09-30 17:02:09 -0400

    fixed new error upon deployment in contactUsPage

--- frontend/src/pages/commonPages/ContactUsPage.tsx
+++ frontend/src/pages/commonPages/ContactUsPage.tsx
@@ -1,12 +1,9 @@
-import { useTranslation } from 'react-i18next';
 import Footer from '../../components/Footer';
 import ContactSection from '../../components/contactUs/ContactSection';
 import ContactHero from '../../components/contactUs/ContactHero';
 import ContactHeader from '../../components/contactUs/ContactHeader';
 
 const ContactUsPage = () => {
-    const { t } = useTranslation();
-
     return (
         <div className="md:pt-16 bg-pale-blue">
             <ContactHero />
